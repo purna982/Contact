dd66cc4b43c293d48bc43c826319d3fa7b72fc49
purna982 <seelampurna2@gmail.com>
2020-12-21 18:47:57 +0530

    purnareset

--- src/main/java/com/purna/stocks/webservices/SwaggerConfig.java
+++ src/main/java/com/purna/stocks/webservices/SwaggerConfig.java
@@ -21,7 +21,7 @@ public class SwaggerConfig {
 			"Purna", "http://www.stocks.com", "seelam.seelamppurna@capgemini.com");
 	
 	public static final ApiInfo DEFAULT_API_INFO = new ApiInfo(
-			"Stock API Title", "Facebook stocks", "1.0",
+			"Stock API Title", "Facebook222dfdfpitere stocks", "1.0",
 			"urn:tos", DEFAULT_CONTACT, 
 			"Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0");
 
